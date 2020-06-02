e8e10cb63c3242ec76f073d077b3728dc13b1d34
polsafont <polsafont@gmail.com>
2020-06-02 19:49:10 +0200

    pas 11.1 flask database orders and accounts table creation

--- PracticaWeb/models/order.py
+++ PracticaWeb/models/order.py
@@ -1,15 +1,12 @@
 from db import db
 
 
-class AccountsModel(db.Model):
-    __tablename__ = 'accounts'
-
-    username = db.Column(db.String(30), primary_key=True, unique=True, nullable=False)
-    password = db.Column(db.String(), nullable=False)
-    # 0 not admin/ 1 is admin
-    is_admin = db.Column(db.Integer, nullable=False)
-    available_money = db.Column(db.Integer)
-    orders = db.relationship('OrdersModel', backref='orders', lazy=True)
+class OrdersModel(db.Model):
+    __tablename__ = 'orders'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), foreign_key='accounts.username', nullable=False)
+    id_event = db.Column(db.Integer, nullable=False)
+    tickets_bought = db.Column(db.Integer, nullable=False)
 
     def __init__(self, username, available_money=200, is_admin=0):
         self.username = username
